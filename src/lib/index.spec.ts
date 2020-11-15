6b729d0bc66ff9aad27132dc77bcbe5cc0aab056
zmxhawrhbg <25387744+zmxhawrhbg@users.noreply.github.com>
2020-11-14 20:31:07 -0700

    write test for demon.from_position

--- src/lib/index.spec.ts
+++ src/lib/index.spec.ts
@@ -2,9 +2,17 @@ import test from 'ava';
 
 import PointercrateClient from '.';
 
+// global client (why not?)
+const client = new PointercrateClient();
+
 test("getting demon by id 1", async t => {
-	const client = new PointercrateClient();
 	const demon = await client.demons.from_id(1);
 
 	t.is(demon.id, 1);
+})
+
+test("getting demon by position 2", async t => {
+	const demon = await client.demons.from_position(2);
+
+	t.is(demon.position, 2);
 })
