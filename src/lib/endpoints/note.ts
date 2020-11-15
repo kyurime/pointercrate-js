4cd68d68d3d1465f40f059aff329e80554f5e263
zmxhawrhbg <25387744+zmxhawrhbg@users.noreply.github.com>
2020-11-14 20:56:03 -0700

    remove skip parameter in note

--- src/lib/endpoints/note.ts
+++ src/lib/endpoints/note.ts
@@ -1,18 +1,16 @@
 export default class Note {
 	id: number;
-	record: number;
 	content: string;
 	transferred: boolean;
 	author?: string;
 	editors: string[];
 
-	constructor({ id, record, content, transferred, author, editors }:
+	constructor({ id, content, transferred, author, editors }:
 		{
-			id: number, record: number, content: string,
-			transferred: boolean, author?: string, editors: string[]
+			id: number, content: string, transferred: boolean,
+			author?: string, editors: string[]
 		}) {
 		this.id = id;
-		this.record = record;
 		this.content = content;
 		this.transferred = transferred;
 		this.editors = editors;
